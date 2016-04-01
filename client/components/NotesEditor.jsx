feba8a535ef7cf4d9f93f1aae7c0f22479b95850
koteho <koteho@ukr.net>
2016-04-01 13:30:09 -0400

    In the middle of the frontend creating. .jsx was added

---
+++ client/components/NotesEditor.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const NoteEditor = React.createClass({ 
+    render() {
+        return <h1> This is NoteEditor.jsx </h1>;
+    }
+});
+
+export default NoteEditor;
---
+++ client/components/NotesGrid.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const NoteGrid = React.createClass({ 
+    render() {
+        return <h1> This is NoteGrid.jsx </h1>;
+    }
+});
+
+export default NoteGrid;
